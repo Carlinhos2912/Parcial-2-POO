95fdc94dc8cc5a97a88cc7678610529b466898b8
CarlosElMejor <129225581+Carlinhos2912@users.noreply.github.com>
2024-03-19 09:21:06 -0500

    mainForm upload
    subida del main form y de los archivo de imagen

--- Sistema de Gestion de Libreria CompuSoft/src/sistema/de/gestion/de/libreria/compusoft/Inventario.java
+++ Sistema de Gestion de Libreria CompuSoft/src/sistema/de/gestion/de/libreria/compusoft/Inventario.java
@@ -3,41 +3,41 @@ package sistema.de.gestion.de.libreria.compusoft;
 import java.util.ArrayList;
 
 public class Inventario implements GestorInventario {
-    
+
     private ArrayList<Libro> stock;
     private ArrayList<Libro> prestamos;
-    
+
     public Inventario() {
     }
-    
+
     public Inventario(ArrayList<Libro> stock, ArrayList<Libro> prestamos) {
         this.stock = stock;
         this.prestamos = prestamos;
     }
-    
+
     public ArrayList<Libro> getStock() {
         return stock;
     }
-    
+
     public void setStock(ArrayList<Libro> stock) {
         this.stock = stock;
     }
-    
+
     public ArrayList<Libro> getPrestamos() {
         return prestamos;
     }
-    
+
     public void setPrestamos(ArrayList<Libro> prestamos) {
         this.prestamos = prestamos;
     }
-    
+
     @Override
     public void agregarAStock(Inventario inventario, Libro libro) {
         Boolean est = false;
         for (Libro muestra : inventario.getStock()) {
             if (muestra.getIsbn().equals(libro.getIsbn())) {
                 muestra.setDisponibles(muestra.getDisponibles() + 1);
-                
+
                 est = true;
                 break;
             }
@@ -46,7 +46,7 @@ public class Inventario implements GestorInventario {
             inventario.getStock().add(libro);
         }
     }
-    
+
     @Override
     public void eliminarDeStock(Inventario inventario, Libro libro) {
         Boolean est = false;
@@ -61,13 +61,13 @@ public class Inventario implements GestorInventario {
             System.out.println("El libro no se encuentra registrado");
         }
     }
-    
+
     @Override
     public void agregarAPrestamos(Inventario inventario, Libro libro) {
         Boolean est = false;
         for (Libro muestra : inventario.getStock()) {
-            if (muestra.getIsbn().equals(libro.getIsbn()) && muestra.consultarDisponibilidad() ) {
-                muestra.setDisponibles(muestra.getDisponibles()-1);
+            if (muestra.getIsbn().equals(libro.getIsbn()) && muestra.consultarDisponibilidad()) {
+                muestra.setDisponibles(muestra.getDisponibles() - 1);
                 inventario.getPrestamos().add(libro); // else try inventario.setPrestamos(inventario.getPrestamos().add(libro));
                 break;
             }
@@ -76,31 +76,21 @@ public class Inventario implements GestorInventario {
             System.out.println("El libro no esta disponible");
         }
     }
-    
+
     @Override
     public void eliminarDePrestamos(Inventario inventario, Libro libro) {
-        
+
         Boolean est = false;
         for (Libro muestra : inventario.getPrestamos()) {
             if (muestra.getIsbn().equals(libro.getIsbn())) {
-                muestra.setDisponibles(muestra.getDisponibles()-1);
+                muestra.setDisponibles(muestra.getDisponibles() - 1);
                 inventario.getPrestamos().remove(libro);
                 break;
             }
         }
         if (!est) {
-            System.out.println("El prestamo del libro "+libro.getTitulo()+" no se encuentra registrado");
+            System.out.println("El prestamo del libro " + libro.getTitulo() + " no se encuentra registrado");
         }
-        
+
     }
-    
-    public static void main(String[] args) {
-        Inventario inv = new Inventario(new ArrayList<>(), new ArrayList<>());
-        inv.agregarAStock(inv, new Libro("titulo", "autor", "isbn", 15000, 10));
-        inv.agregarAStock(inv, new Libro("titulo", "autor", "isbn", 15000, 10));
-        System.out.println("Disponibilidad: " + inv.getStock().get(0).consultarDisponibilidad());
-        inv.eliminarDeStock(inv, new Libro("titulo", "autor", "isbn", 15000, 10));
-        inv.eliminarDeStock(inv, new Libro("titulo", "autor", "isbn", 15000, 10));
-    }
-    
 }
